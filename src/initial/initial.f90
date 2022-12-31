4672a8c0e5d75894ee55d6f910defa400baa5c21
Clara Dehman <dehman@ice.csic.es>
2022-12-31 20:40:16 +0100

    fixing the main bug of the thermal part

--- src/initial/initial.f90
+++ src/initial/initial.f90
@@ -15,14 +15,14 @@
 ! and they return the initial topology of the magnetic field in cubed-sphere coordinates
 !
 !
-!> @author
+!> @Authors:
 !>  Clara Dehman
 !>  Stefano Ascenzi
 !>  Daniele Viganò
 !-------------------------------------------------------------------------------
 
 
-subroutine initial_condition()
+subroutine initial_condition(it, time_read)
 
   use input_params
   use constants, only: PI
@@ -35,11 +35,13 @@ subroutine initial_condition()
   use grid, only: f_spherical_to_cs
   use grid, only: curl_fnvol, fghost
   use grid, only: dot_prod
-  use initial_magnetic, only: binit, binit_axi, DP_TQ, whistler, pureTQ, bessel, test_pa, hotspot
+  use initial_magnetic
   use magnetic_evolution, only: compute_E, compute_joule
 
   implicit none
 
+  integer, intent(out) :: it
+  real*8, intent(out) :: time_read
   real*8, dimension (0:nr+1,0:nang+1,0:nang+1,6) :: xr, xxi, xeta, bth, bphi
   
 
@@ -51,21 +53,23 @@ subroutine initial_condition()
   T_core = T_init   ! Temperature of the core
   q_joule = 0.d0
 
-  !********* non uniform temperature (for debugging) *********
-  !do j = 1, nangt
-  !  do k = 1,nangt
-  !    do p = 1,6
-  !      temp(:,j,k,p) = T_init*(1.d0 + dcos(theta(2*j, 2*k, p)**2))
-  !    enddo
-  !  enddo
-  !enddo
-  !T_core = 1.5*T_init
-  ! ********** END ******************
+  if (resume_checkpnt_number .gt. 0) then 
+
+    if (ibevol) then
+    ! Delete this once the checkpoint for magnetic field is written 
+      print*, 'WARNING: At the moment the resume from checkpoint only contain the thermal evolution: B is set as the initial one'
+    endif
+    call set_from_checkpoint(resume_checkpnt_number, it, time_read)
+
+  else 
+    time_read = 0.d0
+    it = 0
+  endif  
 
   call envelope_model()
 
-  !   Choose the initial magnetic topology.
-  if (bpolmax == 0. .and. btormax == 0.) then
+  ! Set the initial magnetic topology.
+  if (bpol_init == 0. .and. btor_init == 0.) then
     br = 0d0
     bxi = 0d0
     beta = 0d0
@@ -81,30 +85,7 @@ subroutine initial_condition()
 
   else
 
-    select case (init_mag_top)
-      case("Bessel")
-        call bessel(bpolmax,btormax)
-      case ("PerAzo") 
-        call test_pa
-      case ("Hotspot") 
-        call hotspot
-      case ("PureTQ")
-        call pureTQ(btormax)
-      case ("DP+TQ")
-        call  DP_TQ(bpolmax,btormax)
-      case ("Scalar")
-        call  binit(bpolmax,btormax)
-      case ("Scalaxi")
-        call  binit_axi(bpolmax,btormax)
-      case ("Whistler")
-        call  whistler(bpolmax,btormax)
-      case default
-        write(*,*) "<error>", &
-                 & "[B_initial]", &
-                 & "Invalid initial magnetic topology: ", &
-                 & init_mag_top
-      stop
-    end select
+    call binit()
   
    ! Calculation of electric currents
     xr   = 0d0
@@ -138,3 +119,56 @@ subroutine initial_condition()
 
   
 end subroutine initial_condition
+
+!-------------------------------------------------------------
+! set from checkpoint
+!> @brief: This routine read the checkpoint file and fill the variable 
+!> arreys
+!
+!> checkpoint_number[in]  ID number of the checkpoint file from which we want to restart 
+!> it [out]               iteration number at the checkpoint
+!> time_read [out]        time at the checkpoint
+!
+! 
+!> @author:
+!> Stefano Ascenzi
+!-------------------------------------------------------------
+
+subroutine set_from_checkpoint(checkpoint_number, it, time_read)
+
+  use grid, only: nrt, nangt
+  use grid, only: T_core, temp
+
+  implicit none 
+
+  integer, intent(out) :: it
+  real*8, intent(out) :: time_read
+  integer, intent(in) :: checkpoint_number
+  character(len = 50)  :: filename, number_str
+  integer :: i,j,k,p, imin_vis 
+  imin_vis = 0
+
+  write(number_str, "(I3)") checkpoint_number
+
+  filename = "out/3D/checkpoint_"//trim(adjustl(number_str))//".dat"
+
+
+  open (unit = 230, file = filename, status = 'old')
+  
+  read(230,*) it
+  read(230,*) time_read
+  do p=1,6
+    do i=imin_vis+1,nrt
+      do j=0,nangt+1
+        do k=0,nangt+1
+          read(230,*) temp(i,j,k,p)
+        end do
+      end do
+    end do
+  end do
+
+  close(230)
+
+  T_core = temp(1,1,1,1)
+  
+end subroutine set_from_checkpoint
