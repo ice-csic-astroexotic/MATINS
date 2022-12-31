64ec66cfec498f339c77fe4862a120b43841a585
Clara Dehman <dehman@ice.csic.es>
2022-12-31 20:33:35 +0100

    implementing all the modifications to main. thermal part

--- src/main.f90
+++ src/main.f90
@@ -3,27 +3,28 @@ program main
   use input_params
   use grid, only: allocate_grid, build_grid
   use grid, only: nrt, tem0, temp, enu
-  use grid, only: nangt, T_core !for debugging
   use microphysics, only: analytical_microphysics
   use conductivities, only: compute_conductivities
   use magnetic_evolution, only: magnetic_evol, dtb_adaptive
   use thermal_evolution, only: tevol
   use output, only: output_magnetic_1D, output_magnetic_2D, output_surface
   use output, only: output_thermal_1D, output_vtu, output_temperature_cooling_curve
+  use output, only: output_checkpoint
   use OMP_LIB
 
   implicit none
 
   real*8 :: dt, dtb
-  real*8 :: time, time_b
+  real*8 :: time, time_b, time_read
   real*8 :: tau_output_serv, tau_output3D_serv
   integer :: nradial, nangular
-  integer :: ib, it, ic, nout, nout3D, noutT, nout3DT
+  integer :: ib, it, ic, nout, nout3D, noutT, nout3DT, it_read
+  logical :: checkpoint_condition1, checkpoint_condition2
 
-  !Read input and initialize values
+  ! Read input and initialize values
   call read_input_file()
 
-  time = 0.d0 !1.0 this is for Perez-Azorin test!3.d-1
+  time = 0.d0
   time_b = 0.d0
 
   ! Definition of the grid, metric and all common quantities
@@ -35,11 +36,13 @@ program main
   print*,"Grid built"
 
   ! Definition of initial magnetic field and temperature in spherical coordinates
-  call initial_condition()
+  call initial_condition(it_read, time_read)
+  time = time_read
   print*,"Initial conditions set"
 
   ib = 0
-  it = 0
+  it = it_read
+  !it = 0
   if (courant_prefactor .le. 0.) then
     dtb = dtb0
   endif
@@ -48,21 +51,22 @@ program main
  ! Thermal evolution
   do while (time < final_time)
 
-    if (time == 0.d0) then
-      do ic = 1,nrt
-        tem0(ic,:,:,:) = temp(ic,:,:,:)/enu(2*ic-1)
-      enddo 
-    endif
+    !if (time == 0.d0) then
+    do ic = 1,nrt
+      tem0(ic,:,:,:) = temp(ic,:,:,:)/enu(2*ic-1)
+    enddo 
+    !endif
 
      if (itevol .eqv. .false.) then
     !  dt = 1.d-5*final_time 
       dt = 1.01d0*final_time 
     ! Analytical evolution of temperature (from Yakovlev, see Dehman 2022)
-      ! if (time /= 0.d0) then
-      !   do ic = 1,nrt
-      !     tem0(ic,:,:,:) = 6.8695d0/((time*1.d6)**(1.d0/6.d0)*enu(2*ic-1)) 
-      !   enddo
-      ! end if 
+      if (time /= 0.d0) then
+        do ic = 1,nrt
+          tem0(ic,:,:,:) = 6.8695d0/((time*1.d6)**(1.d0/6.d0)*enu(2*ic-1)) 
+        enddo
+      end if 
+
     else
       call adaptive_cooling_timestep(time,dt)
     endif
@@ -92,11 +96,14 @@ program main
 
       noutT = floor(tau_output_serv/dt)
       nout3DT = floor(tau_output3D_serv/dt)
+
+      
       if (noutT == 0) noutT = 1
       if (nout3DT == 0) nout3DT = 1
 
+
       if (modulo(it,noutT) == 0) then
-        call output_thermal_1D(time*1.d6)
+        call output_thermal_1D(time)
         call output_temperature_cooling_curve(it, time, dt)
       end if
 
@@ -104,6 +111,16 @@ program main
         call output_vtu(time,tau_output_serv)
         call output_surface(time)
       end if
+
+      ! Write the checkpoint given by the indicated frequency in the output, if any
+      checkpoint_condition1 = (checkpnt_freq .gt. 0) .and. (mod(it,checkpnt_freq) .eq. 0)
+      ! Avoid writing at t=0 or at the beginning of a resumed run
+      checkpoint_condition2 = (it .gt. resume_checkpnt_number) .and. (time .ne. 0d0)
+
+      if (checkpoint_condition1 .and. checkpoint_condition2) then
+        ! time in simulation unit
+        call output_checkpoint(it, time) 
+      endif 
 
     endif
 
@@ -136,7 +153,6 @@ program main
         end if
     
         call magnetic_evol(dtb, time_b)
-    
         time_b = time_b + dtb
         ib = ib + 1
     
